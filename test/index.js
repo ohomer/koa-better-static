890651aa92d596e46f56c1be816e5278ea3f1128
Nick Baugh <niftylettuce@gmail.com>
2017-09-11 01:32:19 -0400

    Fixed deps and tests

--- test/index.js
+++ test/index.js
@@ -1,264 +1,229 @@
-
 'use strict';
 
 const assert = require('assert');
 const request = require('supertest');
 const serve = require('..');
-const koa = require('koa');
+const Koa = require('koa');
 const mount = require('koa-mount');
 
-describe('serve(root)', function(){
-    describe('when root = "."', function(){
-      it('should serve from cwd', function(done){
-        const app = koa();
+describe('serve(root)', () => {
+	describe('when root = "."', () => {
+		it('should serve from cwd', done => {
+			const app = new Koa();
 
-        app.use(serve('.'));
+			app.use(serve('.'));
 
-        request(app.listen())
-        .get('/package.json')
-        .expect(200, done);
-      })
-    })
+			request(app.listen()).get('/package.json').expect(200, done);
+		});
+	});
 
-    describe('when path is not a file', function(){
-      it('should 404', function(done){
-        const app = koa();
+	describe('when path is not a file', () => {
+		it('should 404', done => {
+			const app = new Koa();
 
-        app.use(serve('test/fixtures'));
+			app.use(serve('test/fixtures'));
 
-        request(app.listen())
-        .get('/something')
-        .expect(404, done);
-      })
-    })
+			request(app.listen()).get('/something').expect(404, done);
+		});
+	});
 
-    describe('when upstream middleware responds', function(){
-      it('should respond', function(done){
-        const app = koa();
+	describe('when upstream middleware responds', () => {
+		it('should respond', done => {
+			const app = new Koa();
 
-        app.use(serve('test/fixtures'));
+			app.use(serve('test/fixtures'));
 
-        app.use(function *(next){
-          yield next;
-          this.body = 'hey';
-        });
+			app.use(function * (next) {
+				yield next;
+				this.body = 'hey';
+			});
 
-        request(app.listen())
-        .get('/hello.txt')
-        .expect(200)
-        .expect('world', done);
-      })
-    })
+			request(app.listen()).get('/hello.txt').expect(200).expect('world', done);
+		});
+	});
 
-    describe('the path is valid', function(){
-      it('should serve the file', function(done){
-        const app = koa();
+	describe('the path is valid', () => {
+		it('should serve the file', done => {
+			const app = new Koa();
 
-        app.use(serve('test/fixtures'));
+			app.use(serve('test/fixtures'));
 
-        request(app.listen())
-        .get('/hello.txt')
-        .expect(200)
-        .expect('world', done);
-      })
-    })
+			request(app.listen()).get('/hello.txt').expect(200).expect('world', done);
+		});
+	});
 
-    describe('.index', function(){
-      describe('when present', function(){
-        it('should alter the index file supported', function(done){
-          const app = koa();
+	describe('.index', () => {
+		describe('when present', () => {
+			it('should alter the index file supported', done => {
+				const app = new Koa();
 
-          app.use(serve('test/fixtures', { index: 'index.txt' }));
+				app.use(serve('test/fixtures', {index: 'index.txt'}));
 
-          request(app.listen())
+				request(app.listen())
           .get('/')
           .expect(200)
           .expect('Content-Type', 'text/plain; charset=utf-8')
           .expect('text index', done);
-        })
-      })
+			});
+		});
 
-      describe('when added', function(){
-        it('should use index.html', function(done){
-          const app = koa();
+		describe('when added', () => {
+			it('should use index.html', done => {
+				const app = new Koa();
 
-          app.use(serve('test/fixtures', { index: 'index.html' }));
+				app.use(serve('test/fixtures', {index: 'index.html'}));
 
-          request(app.listen())
+				request(app.listen())
           .get('/world/')
           .expect(200)
           .expect('Content-Type', 'text/html; charset=utf-8')
           .expect('html index', done);
-        })
-      });
+			});
+		});
 
-      describe('by default', function(){
-        it('should not use index.html', function(done){
-          const app = koa();
+		describe('by default', () => {
+			it('should not use index.html', done => {
+				const app = new Koa();
 
-          app.use(serve('test/fixtures'));
+				app.use(serve('test/fixtures'));
 
-          request(app.listen())
-          .get('/world/')
-          .expect(404, done);
-        })
-      })
-    })
+				request(app.listen()).get('/world/').expect(404, done);
+			});
+		});
+	});
 
-    describe('when method is not `GET` or `HEAD`', function(){
-      it('should 404', function(done){
-        const app = koa();
+	describe('when method is not `GET` or `HEAD`', () => {
+		it('should 404', done => {
+			const app = new Koa();
 
-        app.use(serve('test/fixtures'));
+			app.use(serve('test/fixtures'));
 
-        request(app.listen())
-        .post('/hello.txt')
-        .expect(404, done);
-      })
-    })
+			request(app.listen()).post('/hello.txt').expect(404, done);
+		});
+	});
 
-  describe('option - format', function(){
-    describe('when format: false', function(){
-      it('should 404', function(done){
-        const app = koa();
+	describe('option - format', () => {
+		describe('when format: false', () => {
+			it('should 404', done => {
+				const app = new Koa();
 
-        app.use(serve('test/fixtures', {
-          index: 'index.html',
-          format: false
-        }));
+				app.use(
+          serve('test/fixtures', {
+	index: 'index.html',
+	format: false
+})
+        );
 
-        request(app.listen())
-        .get('/world')
-        .expect(404, done);
-      })
+				request(app.listen()).get('/world').expect(404, done);
+			});
 
-      it('should 200', function(done){
-        const app = koa();
+			it('should 200', done => {
+				const app = new Koa();
 
-        app.use(serve('test/fixtures', {
-          index: 'index.html',
-          format: false
-        }));
+				app.use(
+          serve('test/fixtures', {
+	index: 'index.html',
+	format: false
+})
+        );
 
-        request(app.listen())
-        .get('/world/')
-        .expect(200, done);
-      })
-    })
+				request(app.listen()).get('/world/').expect(200, done);
+			});
+		});
 
-    describe('when format: true', function(){
-      it('should 200', function(done){
-        const app = koa();
+		describe('when format: true', () => {
+			it('should 200', done => {
+				const app = new Koa();
 
-        app.use(serve('test/fixtures', {
-          index: 'index.html',
-          format: true
-        }));
+				app.use(
+          serve('test/fixtures', {
+	index: 'index.html',
+	format: true
+})
+        );
 
-        request(app.listen())
-        .get('/world')
-        .expect(200, done);
-      })
+				request(app.listen()).get('/world').expect(200, done);
+			});
 
-      it('should 200', function(done){
-        const app = koa();
+			it('should 200', done => {
+				const app = new Koa();
 
-        app.use(serve('test/fixtures', {
-          index: 'index.html',
-          format: true
-        }));
+				app.use(
+          serve('test/fixtures', {
+	index: 'index.html',
+	format: true
+})
+        );
 
-        request(app.listen())
-        .get('/world/')
-        .expect(200, done);
-      })
-    })
-  });
+				request(app.listen()).get('/world/').expect(200, done);
+			});
+		});
+	});
 
+	describe('Support if-modified-since', () => {
+		it('should 304', done => {
+			const app = new Koa();
 
-  describe('Support if-modified-since', function(){
-    it('should 304', function(done){
-      const app = koa();
+			app.use(serve('test/fixtures'));
 
-      app.use(serve('test/fixtures'));
-
-      request(app.listen())
+			request(app.listen())
         .get('/world/index.html')
         .expect(200)
-        .end(function(err, response) {
-          if (err)
-            done(err);
+        .end((err, response) => {
+	if (err) {
+		done(err);
+	}
 
+	const lastModified = response.headers['last-modified'];
 
-          var lastModified = response.headers['last-modified'];
-
-
-          request(app.callback())
+	request(app.callback())
             .get('/world/index.html')
             .set('if-modified-since', lastModified)
             .expect(304, done);
+});
+		});
 
-        });
-    });
+		it('should 200', done => {
+			const app = new Koa();
 
+			app.use(serve('test/fixtures'));
 
-    it('should 200', function(done){
-      const app = koa();
-
-      app.use(serve('test/fixtures'));
-
-      request(app.listen())
+			request(app.listen())
         .get('/world/index.html')
         .set('if-modified-since', 'Mon Jan 18 2011 23:04:34 GMT-0600')
-        .expect(200, done)
-    });
-  });
+        .expect(200, done);
+		});
+	});
 
-  describe('Work with koa-mount', function() {
+	describe('Work with koa-mount', () => {
+		it('should mount fine', done => {
+			const app = new Koa();
 
-    it('should mount fine', function(done) {
+			app.use(
+        mount('/fixtures', serve(require('path').join(__dirname, '/fixtures')))
+      );
 
-      const app = koa();
-
-      app.use(
-        mount('/fixtures',
-        serve(require('path').join(__dirname, '/fixtures'))
-      ));
-
-
-      request(app.listen())
+			request(app.listen())
         .get('/fixtures/hello.txt')
         .expect(200)
-        .end(function(err, data) {
-         // console.log('Got response: ', err, data);
-          done(err);
-        })
-
-
-
-    });
-
-  });
-
+        .end((err, data) => {
+          // Console.log('Got response: ', err, data);
+	done(err);
+});
+		});
+	});
 });
 
 // This is more of a test of js, than of the logic. But something we rely on
-describe('Dates should truncate not, round', function() {
+describe('Dates should truncate not, round', () => {
+	it('should mount fine', () => {
+		const str = new Date().toUTCString();
 
-  it('should mount fine', function() {
+		let ms = Date.parse(str);
+		ms += 999; // Add 999 ms
 
-    var str = new Date().toUTCString();
+		const nd = new Date(ms);
 
-    let ms = Date.parse(str);
-    ms += 999; // add 999 ms
-
-
-    let nd = new Date(ms);
-
-    assert(nd.toUTCString() === str);
-
-  });
-
-
+		assert(nd.toUTCString() === str);
+	});
 });
-
